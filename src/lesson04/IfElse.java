58898208ebdb88fe73314ada2748cf0a84d3ef8f
andrew <perepelkin.work@ya.ru>
2018-05-28 19:34:04 +0300

    lesson04 changes

--- src/lesson04/IfElse.java
+++ src/lesson04/IfElse.java
@@ -1,18 +1,24 @@
 package lesson04;
 
+import java.util.Scanner;
+
 public class IfElse {
 
     public static void main(String[] args) {
 
+        int a;
         if (args.length > 0) {
-            int a = Integer.parseInt(args[0]);
-
-            if (a % 2 == 0)
-                System.out.println("Четное");
-            else
-                System.out.println("Нечетное");
+            a = Integer.parseInt(args[0]);
         } else {
-            System.out.println("Число не задано");
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Введите число:");
+            a = scanner.nextInt();
         }
+
+        if (a % 2 == 0)
+            System.out.println("Четное");
+        else
+            System.out.println("Нечетное");
     }
 }
